d92d483c87f6a3754efd247a79123af06a51996f
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-01-04 12:32:57 +0100

    form

--- src/components/MainContact.jsx
+++ src/components/MainContact.jsx
@@ -7,15 +7,15 @@ function MainContact() {
             <h2 className="mainContact__h2">Contacta conmigo</h2>
             <form className="mainContact__form" action="https://formsubmit.co/m.teresacastrogonzalez@gmail.com" method="POST">
                 <label className="mainContact__form--label" htmlFor="email">E-mail
-                    <input className="mainContact__form--input" name="email" id="email" type="text" placeholder="email@correo.com" />
+                    <input className="mainContact__form--input" name="email" id="email" type="email" placeholder="email@correo.com" required />
                 </label>
 
                 <label className="mainContact__form--label" htmlFor="subject">Asunto
-                    <input className="mainContact__form--input" name="subject" id="subject" type="text" placeholder="Tema a tratar..." />
+                    <input className="mainContact__form--input" name="subject" id="subject" type="text" placeholder="Tema a tratar..." required />
                 </label>
 
-                <label className="mainContact__form--label textarea" htmlFor="contact">Mensaje
-                    <textarea className="mainContact__form--textarea" name="contact" id="contact" cols="30" rows="10" placeholder="Estamos interesados en tu perfil..." type="text"></textarea>
+                <label className="mainContact__form--label textarea" htmlFor="message">Mensaje
+                    <textarea className="mainContact__form--textarea" name="message" id="contact" cols="30" rows="10" placeholder="Estamos interesados en tu perfil..." type="text" required></textarea>
                 </label>
 
                 <button type="submit">Enviar</button>
