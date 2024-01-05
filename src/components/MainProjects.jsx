0d0f79c3d7feb8ef4d141cb4ef2e9a8f5aeea55a
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-01-05 10:42:56 +0100

    hover container

--- src/components/MainProjects.jsx
+++ src/components/MainProjects.jsx
@@ -11,15 +11,15 @@ function MainProjects() {
             <h3 className="mainProjects__h3">&lt;h3&gt; Cada línea de código te acerca un paso más a tu <span className="mainProjects__h3--span">meta</span> &lt;/h3&gt;</h3>
             <div className="mainProject__div">
                 <article className="mainProjects__art">
-                    <div className="mainProjects__art--img-container">
-                        <img className="mainProjects__art--img" src={school} alt="" />
-                        <div className="mainProjects__art--div">
-                            <h4 className="mainProjects__art--div-h4">Título proyecto</h4>
-                            <ul className="mainProjects__art--div-ul">
-                                <li><img className="div__ul--li" src="../src/images/html-48.png" alt="logo html" /></li>
-                                <li><img className="div__ul--li" src="../src/images/css-48.png" alt="logo css" /></li>
-                                <li><img className="div__ul--li" src="../src/images/sass-48.png" alt="logo sass" /></li>
-                                <li><img className="div__ul--li" src="" alt="" /></li>
+                    <div className="mainProjects__art--container">
+                        <img className="mainProjects__art--container-img" src={school} alt="" />
+                        <div className="mainProjects__art--container-div">
+                            <h4 className="art__div--h4">Título proyecto</h4>
+                            <ul className="art__div--ul">
+                                <li><img className="art__div--ul-img" src="../src/images/html-48.png" alt="logo html" /></li>
+                                <li><img className="art__div--ul-img" src="../src/images/css-48.png" alt="logo css" /></li>
+                                <li><img className="art__div--ul-img" src="../src/images/sass-48.png" alt="logo sass" /></li>
+                                <li><img className="art__div--ul-img" src="" alt="" /></li>
                             </ul>
                         </div>
                     </div>
