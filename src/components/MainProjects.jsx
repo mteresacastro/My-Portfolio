fb8eaf6c664adce46154de8fc5edbecb50d57c27
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-01-05 10:24:33 +0100

    work in projects

--- src/components/MainProjects.jsx
+++ src/components/MainProjects.jsx
@@ -1,5 +1,5 @@
 import '../styles/mainProject.scss';
-
+import school from '../../src/images/backtoschool.png'
 
 import milogo from '../../src/assets/miLogoMorado.png'
 
@@ -7,28 +7,36 @@ function MainProjects() {
 
     return (
         <section className="mainProjects" id="mainProjects">
-            <h2>Proyectos</h2>
-            <h3>&lt;h3&gt; Cada línea de código te acerca un paso más a tu <span>meta</span> &lt;/h3&gt;</h3>
-            <article>
-                <img src="" alt="" />
-                <h4>Título proyecto</h4>
-                <ul>
-                    <li><img src="../src/images/css-48.png" alt="logo css"></img></li>
-                    <li><img src="" alt=""></img></li>
-                    <li><img src="" alt=""></img></li>
-                    <li><img src="" alt=""></img></li>
-                </ul>
-            </article>
-            <article>
-                <img src="" alt="" />
-                <h4>Título proyecto 2</h4>
-                <ul>
-                    <li><img src="../src/images/css-48.png" alt="logo css"></img></li>
-                    <li><img src="" alt=""></img></li>
-                    <li><img src="" alt=""></img></li>
-                    <li><img src="" alt=""></img></li>
-                </ul>
-            </article>
+            <h2 className="mainProjects__h2">Proyectos</h2>
+            <h3 className="mainProjects__h3">&lt;h3&gt; Cada línea de código te acerca un paso más a tu <span className="mainProjects__h3--span">meta</span> &lt;/h3&gt;</h3>
+            <div className="mainProject__div">
+                <article className="mainProjects__art">
+                    <div className="mainProjects__art--img-container">
+                        <img className="mainProjects__art--img" src={school} alt="" />
+                        <div className="mainProjects__art--div">
+                            <h4 className="mainProjects__art--div-h4">Título proyecto</h4>
+                            <ul className="mainProjects__art--div-ul">
+                                <li><img className="div__ul--li" src="../src/images/html-48.png" alt="logo html" /></li>
+                                <li><img className="div__ul--li" src="../src/images/css-48.png" alt="logo css" /></li>
+                                <li><img className="div__ul--li" src="../src/images/sass-48.png" alt="logo sass" /></li>
+                                <li><img className="div__ul--li" src="" alt="" /></li>
+                            </ul>
+                        </div>
+                    </div>
+                </article>
+                <article className="mainProjects__art">
+                    <img className="mainProjects__art--img" src="" alt="" />
+                    <div className="mainProjects__art--div">
+                        <h4 className="mainProjects__art--div-h4">Título proyecto</h4>
+                        <ul className="mainProjects__art--div-ul">
+                            <li><img className="div__ul--li" src="../src/images/css-48.png" alt="logo css"></img></li>
+                            <li><img className="div__ul--li" src="" alt=""></img></li>
+                            <li><img className="div__ul--li" src="" alt=""></img></li>
+                            <li><img className="div__ul--li" src="" alt=""></img></li>
+                        </ul>
+                    </div>
+                </article>
+            </div>
         </section>
     );
 }
