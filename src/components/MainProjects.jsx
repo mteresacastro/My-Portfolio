35653670d85f380e499047b11b1a61e3abe40a8b
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-01-07 12:16:38 +0100

    visibility

--- src/components/MainProjects.jsx
+++ src/components/MainProjects.jsx
@@ -23,6 +23,19 @@ function MainProjects() {
                         </div>
                     </div>
                 </article>
+                <article className="mainProjects__art">
+                    <div className="mainProjects__art--container">
+                        <img className="mainProjects__art--container-img" src={school} alt="" />
+                        <div className="mainProjects__art--container-div">
+                            <h4 className="art__div--h4">Ver Demo</h4>
+                            <ul className="art__div--ul">
+                                <li><img className="art__div--ul-img" src="../src/images/html-48.png" alt="logo html" /></li>
+                                <li><img className="art__div--ul-img" src="../src/images/css-48.png" alt="logo css" /></li>
+                                <li><img className="art__div--ul-img" src="../src/images/sass-48.png" alt="logo sass" /></li>
+                            </ul>
+                        </div>
+                    </div>
+                </article>
 
             </div>
         </section>
