9469f9350752f13ca48e5d8400516f5571e1dd70
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-01-08 10:37:36 +0100

    add details project

--- src/components/MainProjects.jsx
+++ src/components/MainProjects.jsx
@@ -25,9 +25,37 @@ function MainProjects() {
                                 </ul>
                             </div>
                         </div>
-                        <h4 className="mainProjects__art--title">Back to school - Web estática</h4>
+                    </article>
+                    <a href="https://github.com/mteresacastro/Static-Website-Team-Presentation-HTML-CSS" target='_blank' rel='noreferrer'>
+                        <div className="mainProjects__art--titleGit">
+                            <i className="mainProjects__art--titleGit-git fa-brands fa-github"></i>
+                            <h4 className="mainProjects__art--titleGit-title">Back to school - Web estática</h4>
+                        </div>
+                    </a>
+                </a>
+                <a href="https://mteresacastro.github.io/Static-Website-Team-Presentation-HTML-CSS/" target='_blank' rel='noreferrer'>
+                    <article className="mainProjects__art">
+                        <div className="mainProjects__art--container">
+                            <img className="mainProjects__art--container-img" src={school} alt="" />
+                            <div className="mainProjects__art--container-div">
+                                <h4 className="art__div--h4">Ver Demo</h4>
+                                <p className="art__div--p">Web estática desarrollada en equipo según metodología Agile. Diseño responsive.</p>
+                                <ul className="art__div--ul">
+                                    <li><img className="art__div--ul-img" src="../src/images/html-48.png" alt="logo html" /></li>
+                                    <li><img className="art__div--ul-img" src="../src/images/css-48.png" alt="logo css" /></li>
+                                    <li><img className="art__div--ul-img" src="../src/images/sass-48.png" alt="logo sass" /></li>
+                                    <li><img className="art__div--ul-img" src="../src/images/scrum-48.png" alt="logo scrum" /></li>
+                                </ul>
+                            </div>
+                        </div>
 
                     </article>
+                    <a href="https://github.com/mteresacastro/Static-Website-Team-Presentation-HTML-CSS" target='_blank' rel='noreferrer'>
+                        <div className="mainProjects__art--titleGit">
+                            <i className="mainProjects__art--titleGit-git fa-brands fa-github"></i>
+                            <h4 className="mainProjects__art--titleGit-title">Team Presentation - Web estática</h4>
+                        </div>
+                    </a>
                 </a>
                 <a href="https://mteresacastro.github.io/Static-Website-BackToSchool-HTML-CSS/" target='_blank' rel='noreferrer'>
                     <article className="mainProjects__art">
@@ -44,6 +72,12 @@ function MainProjects() {
                             </div>
                         </div>
                     </article>
+                    <a href="https://github.com/mteresacastro/Static-Website-Team-Presentation-HTML-CSS" target='_blank' rel='noreferrer'>
+                        <div className="mainProjects__art--titleGit">
+                            <i className="mainProjects__art--titleGit-git fa-brands fa-github"></i>
+                            <h4 className="mainProjects__art--titleGit-title">Team Presentation - Web estática</h4>
+                        </div>
+                    </a>
                 </a>
                 <a href="https://mteresacastro.github.io/Static-Website-BackToSchool-HTML-CSS/" target='_blank' rel='noreferrer'>
                     <article className="mainProjects__art">
@@ -60,6 +94,12 @@ function MainProjects() {
                             </div>
                         </div>
                     </article>
+                    <a href="https://github.com/mteresacastro/Static-Website-Team-Presentation-HTML-CSS" target='_blank' rel='noreferrer'>
+                        <div className="mainProjects__art--titleGit">
+                            <i className="mainProjects__art--titleGit-git fa-brands fa-github"></i>
+                            <h4 className="mainProjects__art--titleGit-title">Team Presentation - Web estática</h4>
+                        </div>
+                    </a>
                 </a>
             </div>
         </section>
