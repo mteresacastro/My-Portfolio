012fda458e0d0a15d993f5eaa9dd9c27e26aba6b
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-01-12 16:09:36 +0100

    fix aboutme

--- src/components/MainAboutMe.jsx
+++ src/components/MainAboutMe.jsx
@@ -6,7 +6,7 @@ function MainAboutMe() {
     return (
         <section className="mainAboutMe">
             <h2 className="mainAboutMe__h2">Algo más sobre mí...</h2>
-            <p className="mainAboutMe__desc" > ¡Gracias por llegar hasta aquí! ¿Miedo? Esa palabra no va conmigo, pero sí crecer y reinventarme. Tras varios años en Sanidad decidí dar un vuelco a mi carrera profesional, y con la ilusión que me caracteriza comencé esta nueva aventura. Soy perseverante, no me rindo fácilmente y por ello siento que crezco día a día en este nuevo océano de oportunidades. Apasionada del aprendizaje, siempre busco maneras de mejorar mis habilidades. En resumen, soy una persona apasionada, perseverante, creativa y resolutiva que está emocionada de seguir creciendo en el campo del desarrollo web. </p>
+            <p className="mainAboutMe__desc" > ¡Gracias por llegar hasta aquí! ¿Miedo? Esa palabra no va conmigo, pero sí crecer y reinventarme. Tras varios años en Sanidad decidí dar un vuelco a mi carrera profesional, y con la ilusión que me caracteriza comencé esta nueva aventura. Soy tenaz, no me rindo fácilmente y por ello siento que crezco día a día en este nuevo océano de oportunidades. Apasionada del aprendizaje, siempre busco maneras de mejorar mis habilidades. En resumen, soy una persona apasionada, perseverante, creativa y resolutiva que está emocionada de seguir creciendo en el campo del desarrollo web. </p>
             <hr className="mainAboutMe__deco" id="mainContact" />
         </section>
     );
