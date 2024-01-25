23421654d93c36ae5b6b171a475d376aa3d38ddc
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-01-25 15:15:59 +0100

    add guitarSI

--- src/components/MainProjects.jsx
+++ src/components/MainProjects.jsx
@@ -7,6 +7,7 @@ import profileCards from '../../src/images/profileCards.png'
 import rockPaperScissors from '../../src/images/rockPaperScissors.png'
 import hangmanGame from '../../src/images/hangmanGame.png'
 import owen from '../../src/images/owen.png'
+import guitarSI from '../../src/images/guitarSI.png'
 
 import html from '../../src/images/html-48.png'
 import css from '../../src/images/css-48.png'
@@ -14,16 +15,18 @@ import sass from '../../src/images/sass-48.png'
 import javascript from '../../src/images/javascript-48.png'
 import react from '../../src/images/react-48.png'
 import scrum from '../../src/images/scrum-48.png'
+import vue from '../../src/images/vue-48.png'
 
 
 function MainProjects() {
     const logos = [
-        { src: { html }, alt: 'logo html', project: ['Back to school', 'Team Presentation', 'Spotify', 'Profile Cards', 'Piedra, Papel, Tijera', 'Juego del ahorcado', 'Owen Wilson`s WOW!'] },
-        { src: { css }, alt: 'logo css', project: ['Back to school', 'Team Presentation', 'Spotify', 'Profile Cards', 'Piedra, Papel, Tijera', 'Juego del ahorcado', 'Owen Wilson`s WOW!'] },
+        { src: { html }, alt: 'logo html', project: ['Back to school', 'Team Presentation', 'Spotify', 'Profile Cards', 'Piedra, Papel, Tijera', 'Juego del ahorcado', 'Owen Wilson`s WOW!', 'GuitarSI e-commerce'] },
+        { src: { css }, alt: 'logo css', project: ['Back to school', 'Team Presentation', 'Spotify', 'Profile Cards', 'Piedra, Papel, Tijera', 'Juego del ahorcado', 'Owen Wilson`s WOW!', 'GuitarSI e-commerce'] },
         { src: { sass }, alt: 'logo sass', project: ['Back to school', 'Team Presentation', 'Spotify', 'Profile Cards', 'Piedra, Papel, Tijera', 'Juego del ahorcado', 'Owen Wilson`s WOW!'] },
-        { src: { javascript }, alt: 'logo javascript', project: ['Profile Cards', 'Piedra, Papel, Tijera', 'Juego del ahorcado', 'Owen Wilson`s WOW!'] },
+        { src: { javascript }, alt: 'logo javascript', project: ['Profile Cards', 'Piedra, Papel, Tijera', 'Juego del ahorcado', 'Owen Wilson`s WOW!', 'GuitarSI e-commerce'] },
         { src: { react }, alt: 'logo react', project: ['Juego del ahorcado', 'Owen Wilson`s WOW!'] },
-        { src: { scrum }, alt: 'logo scrum', project: ['Team Presentation', 'Profile Cards'] }
+        { src: { scrum }, alt: 'logo scrum', project: ['Team Presentation', 'Profile Cards'] },
+        { src: { vue }, alt: 'logo react', project: ['GuitarSI e-commerce']}
     ];
 
     return (
@@ -32,6 +35,9 @@ function MainProjects() {
             <h3 className="mainProjects__h3">&lt;h3&gt; Cada línea de código te acerca un paso más a tu <span className="mainProjects__h3--span">meta</span> &lt;/h3&gt;</h3>
 
             <div className="mainProject__div box">
+
+                <Article demoLink={"https://mteresacastro.github.io/guitarSI-vue/"} imageSrc={guitarSI} description={'E-commerce de guitarras desarrollado con Vue.js utilizando la Composition API.'} logos={logos} codeLink={"https://github.com/mteresacastro/guitarSI-vue"} title={'GuitarSI e-commerce'} />
+            
                 <Article demoLink={"https://mteresacastro.github.io/Static-Website-BackToSchool-HTML-CSS/"} imageSrc={school} description={'Web estática desarrollada según un diseño de Zeplin preestablecido. Diseño responsive.'} logos={logos} codeLink={"https://github.com/mteresacastro/Static-Website-BackToSchool-HTML-CSS"} title={'Back to school'} />
 
                 <Article demoLink={"https://mteresacastro.github.io/Static-Website-Team-Presentation-HTML-CSS/"} imageSrc={team} description={'Web estática desarrollada en equipo según metodología Agile. Diseño responsive.'} logos={logos} codeLink={"https://github.com/mteresacastro/Static-Website-Team-Presentation-HTML-CSS"} title={'Team Presentation'} />
